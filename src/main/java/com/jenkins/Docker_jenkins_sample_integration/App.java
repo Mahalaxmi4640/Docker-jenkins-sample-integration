5c4c286a37a13f337889e51eb087d89d5495bf24
Mahalaxmi <mahalaxmi.bgr@gmail.com>
2021-12-08 10:06:09 +0530

    First commit

---
+++ src/main/java/com/jenkins/Docker_jenkins_sample_integration/App.java
@@ -0,0 +1,13 @@
+package com.jenkins.Docker_jenkins_sample_integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/jenkins/Docker_jenkins_sample_integration/AppTest.java
@@ -0,0 +1,20 @@
+package com.jenkins.Docker_jenkins_sample_integration;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
